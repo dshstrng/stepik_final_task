837eb7611d2c4b77c0520e253d084312fd404972
daria <yukhnina.d@gmail.com>
2023-07-23 16:11:26 +0300

    add test_guest_can_go_to_login_page

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: '--browser_name=chrome' or '--browser_name=firefox'")
+
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: '--language=en' or '--language=ru'")
+
+
+@pytest.fixture(scope="function")
+def driver():
+    print("\nstart browser for test..")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    yield driver
+    print("\nquit browser..")
+    driver.quit()
